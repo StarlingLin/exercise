984bb07884f0ac457dc3bd105924593bbe5f2ea1
椋鸟Starling <2222040903@qq.com>
2023-10-26 21:23:37 +0800

    练习

---
+++ exercise_23_10_26_5_nowcoder_BC92/exercise_23_10_26_5_nowcoder_BC92/exercise_23_10_26_5_nowcoder_BC92.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };
+	for (int i = 9; i >= 0; i--)
+		scanf("%d", &arr[i]);
+	for (int i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
