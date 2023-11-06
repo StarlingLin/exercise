7908e811cfae4aec19d32113c0cf05e319113d37
椋鸟Starling <2222040903@qq.com>
2023-11-06 18:24:36 +0800

    练习

---
+++ C/exercise_23_11_06_2_统计数字二进制中1的个数/exercise_23_11_06_2_统计数字二进制中1的个数/exercise_23_11_06_2_统计数字二进制中1的个数.c
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int num = 114;
+//    unsigned rnum = (unsigned)num;
+//    int count = 0;//计数
+//    while (rnum)
+//    {
+//        if (rnum % 2 == 1)
+//            count++;
+//        rnum /= 2;
+//    }
+//    printf("二进制中1个数 = %d\n", count);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int num = -1;
+//    int i = 0;
+//    int count = 0;//计数
+//
+//    for (i = 0; i < 32; i++)
+//        if (num & (1 << i))//或者写成((num >> i) & 1)也一样
+//            count++;
+//    printf("二进制中1的个数= %d\n", count);
+//    return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int num = -1;
+	int i = 0;
+	int count = 0;//计数
+	while (num)
+	{
+		count++;
+		num = num & (num - 1);
+	}
+	printf("二进制中1的个数= %d\n", count);
+	return 0;
+}
