47959f9676fc119a0c3759a4fe8b64f8187b1b31
椋鸟Starling <2222040903@qq.com>
2023-11-13 23:52:41 +0800

    练习

--- C/exercise_23_11_13_7_递归顺序查找/exercise_23_11_13_7_递归顺序查找/exercise_23_11_13_7_递归顺序查找.c
+++ C/exercise_23_11_13_7_递归顺序查找/exercise_23_11_13_7_递归顺序查找/exercise_23_11_13_7_递归顺序查找.c
@@ -2,24 +2,37 @@
 
 #include <stdio.h>
 
-int Search(int arr[], int n, int key)
+int n = 0, arr[100] = { 0 }, key = 0, p = 0, ret = 0;
+
+int Search(int arr[], int n, int key, int p)
 {
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] == key)
-			return i;
-	}
-	return -1;
+	if (arr[p] == key)
+		return p;
+	if (p == n)
+		return -1;
+	else
+		return Search(arr, n, key, p + 1);
 }
+
+////////////////////////////////////////
+//int Search(int arr[], int n, int key)
+//{
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] == key)
+//			return i;
+//	}
+//	return -1;
+//}
+////////////////////////////////////////
 
 int main()
 {
-	int n = 0, arr[100] = { 0 }, key = 0, ret = 0;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &arr[i]);
 	scanf("%d", &key);
-	ret = Search(arr, n, key);
+	ret = Search(arr, n, key, p);
 	if (-1 == ret)
 		printf("NULL\n");
 	else
