c6df80ba18abaae6789a6c63d89b5ed3f5d46feb
椋鸟Starling <2222040903@qq.com>
2023-11-18 19:58:12 +0800

    练习

--- C/exercise_23_11_18_2_字符指针、字符串测试/exercise_23_11_18_2_字符指针、字符串测试/exercise_23_11_18_2_字符指针、字符串测试.c
+++ C/exercise_23_11_18_2_字符指针、字符串测试/exercise_23_11_18_2_字符指针、字符串测试/exercise_23_11_18_2_字符指针、字符串测试.c
@@ -12,9 +12,30 @@
 
 #include <stdio.h>
 
+//int main()
+//{
+//    const char* pch = "abc";
+//    printf("%s", pch);
+//    return 0;
+//}
+
+#include <stdio.h>
+
 int main()
 {
-    const char* pch = "abc";
-    printf("%s", pch);
+    char str1[] = "abcdef";
+    char str2[] = "abcdef";
+    const char* str3 = "abcdef";    //const可省略
+    const char* str4 = "abcdef";    //const可省略
+
+    if (str1 == str2)
+        printf("str1 and str2 are same\n");
+    else
+        printf("str1 and str2 are not same\n");
+    if (str3 == str4)
+        printf("str3 and str4 are same\n");
+    else
+        printf("str3 and str4 are not same\n");
+
     return 0;
 }
