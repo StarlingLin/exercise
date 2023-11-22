9d1282fa9333660184c6d5aad27e68b008782191
椋鸟Starling <2222040903@qq.com>
2023-11-22 23:42:43 +0800

    练习

--- C/exercise_23_11_22_1_快读快写/exercise_23_11_22_1_快读快写/exercise_23_11_22_1_快读快写.c
+++ C/exercise_23_11_22_1_快读快写/exercise_23_11_22_1_快读快写/exercise_23_11_22_1_快读快写.c
@@ -2,29 +2,38 @@
 
 #include <stdio.h>
 
-////5 times faster than scanf
-//inline int read()
-//{
-//    int x = 0, f = 1;
-//    char ch = getchar();
-//    while (ch < '0' || ch>'9')
-//    {
-//        if (ch == '-')
-//            f = -1;
-//        ch = getchar();
-//    }
-//    while (ch >= '0' && ch <= '9')
-//        x = x * 10 + ch - '0', ch = getchar();
-//    return x * f;
-//}
+//5 times faster than scanf
+inline int read()
+{
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch>'9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + (ch ^ 48), ch = getchar();
+    return x * f;
+}
 
-#define read(a) {char c;while((c=getchar())>47) a=a*10+(c^48);}
+void write(int x)
+{
+    if (x < 0) putchar('-'), x = -x;
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+    return;
+}
+
+////only positive
+//#define read(a) {char c;while((c=getchar())>47) a=a*10+(c^48);}
 
 int main()
 {
-    int a = 0;
-    read(a);
-    printf("%d\n", a);
+    int a = read();
+    printf("%d", a);
+    write(a);
 
     return main();
 }
