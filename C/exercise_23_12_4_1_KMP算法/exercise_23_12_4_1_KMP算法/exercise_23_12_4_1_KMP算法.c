81c073258807dfa20a6d5a8419287c0adc1f80b1
椋鸟Starling <2222040903@qq.com>
2023-12-04 23:42:57 +0800

    练习

--- C/exercise_23_12_4_1_KMP算法/exercise_23_12_4_1_KMP算法/exercise_23_12_4_1_KMP算法.c
+++ C/exercise_23_12_4_1_KMP算法/exercise_23_12_4_1_KMP算法/exercise_23_12_4_1_KMP算法.c
@@ -1,8 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+
+void GetNext(const char*, int*);
 
 char* BF(const char* str1, const char* str2)
 {
@@ -23,18 +26,48 @@ char* BF(const char* str1, const char* str2)
 	return NULL;
 }
 
-char* KMP(const char* str1, const char* str2)
+char* KMP(const char* s, const char* p)
 {
-
-
+	assert(s && p);
+	int i = 0, j = 0;
+	int sLen = (int)strlen(s);
+	int pLen = (int)strlen(p);
+	int* next = (int*)malloc(sizeof p);
+	GetNext(p, next);
+	while (i < sLen && j < pLen)
+	{
+		if (j == -1 || s[i] == p[j])
+			++i, ++j;
+		else
+			j = next[j];
+	}
+	return (j == pLen) ? (char*)&s[i - j] : NULL;
 }
 
 int main()
 {
 	char str1[] = "ababcabcdabcde";
 	char str2[] = "abcd";
-	char* s = BF(str1, str2);
+	//char* s = BF(str1, str2);
+	char* s = KMP(str1, str2);
 	strncpy(s, "@@@@", 4);
 	printf("%s", str1);
 	return 0;
+}
+
+void GetNext(const char* p, int next[])
+{
+	int pLen = (int)strlen(p);
+	next[0] = -1;
+	int k = -1, j = 0;
+	while (j < pLen - 1)
+	{
+		if (k == -1 || p[j] == p[k])
+		{
+			++j, ++k;
+			next[j] = (p[j] != p[k]) ? k : next[k];
+		}
+		else
+			k = next[k];
+	}
 }
