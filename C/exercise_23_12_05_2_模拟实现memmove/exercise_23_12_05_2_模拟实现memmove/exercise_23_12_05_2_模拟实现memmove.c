c8adfc4cb5b4f4fb2c8491081f3180bf35b338dc
椋鸟Starling <2222040903@qq.com>
2023-12-05 20:46:36 +0800

    练习

---
+++ C/exercise_23_12_05_2_模拟实现memmove/exercise_23_12_05_2_模拟实现memmove/exercise_23_12_05_2_模拟实现memmove.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+void* my_memmove(void* dest, const void* src, size_t n)
+{
+	void* ret = dest;
+	if (dest <= src)
+	{
+		while (n--)
+		{
+			*(char*)dest = *(char*)src;
+			((char*)dest)++, ((char*)src)++;
+		}
+	}
+	else
+	{
+		dest = (char*)dest + n - 1;
+		src = (char*)src + n - 1;
+		while (n--)
+		{
+			*(char*)dest = *(char*)src;
+			((char*)dest)--, ((char*)src)--;
+		}
+	}
+	return ret;
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//memmove(arr + 2, arr, 20);
+	my_memmove(arr + 2, arr, 20);
+	for (int i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
