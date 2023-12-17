55be044acede58c9a7dac9281c114ea427e25b12
椋鸟Starling <2222040903@qq.com>
2023-12-17 18:37:23 +0800

    练习

---
+++ C/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+#pragma pack(1)//设置默认对⻬数为1
+
+struct S
+{
+	char c1;
+	int i;
+	char c2;
+};
+
+#pragma pack()//取消设置的对⻬数，还原为默认
+
+int main()
+{
+	printf("%d\n", sizeof(struct S));
+	return 0;
+}
