e86e034e2add5137fcbb2511c21e473148cef3c8
椋鸟Starling <2222040903@qq.com>
2023-12-17 21:32:07 +0800

    练习

--- C/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数.c
+++ C/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数/exercise_23_12_17_2_修改默认对齐数.c
@@ -2,19 +2,56 @@
 
 #include <stdio.h>
 
-#pragma pack(1)//设置默认对⻬数为1
+//#pragma pack(1)//设置默认对⻬数为1
+//
+//struct S
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//
+//#pragma pack()//取消设置的对⻬数，还原为默认
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct S));
+//	return 0;
+//}
 
-struct S
+
+
+#pragma pack(4)
+
+int main(int argc, char* argv[])
 {
-	char c1;
-	int i;
-	char c2;
-};
+	struct tagTest1
+	{
+		short a;
+		char d;
+		long b;
+		long c;
+	};
+	struct tagTest2
+	{
+		long b;
+		short c;
+		char d;
+		long a;
+	};
+	struct tagTest3
+	{
+		short c;
+		long b;
+		char d;
+		long a;
+	};
+	struct tagTest1 stT1;
+	struct tagTest2 stT2;
+	struct tagTest3 stT3;
 
-#pragma pack()//取消设置的对⻬数，还原为默认
-
-int main()
-{
-	printf("%d\n", sizeof(struct S));
+	printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
 	return 0;
 }
+
+#pragma pack()
