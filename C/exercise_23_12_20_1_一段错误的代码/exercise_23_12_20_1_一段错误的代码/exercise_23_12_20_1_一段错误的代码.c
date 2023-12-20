ea33d850f6b1d94b8177164774db6c2fb5a480c3
椋鸟Starling <2222040903@qq.com>
2023-12-20 20:34:31 +0800

    练习

---
+++ C/exercise_23_12_20_1_一段错误的代码/exercise_23_12_20_1_一段错误的代码/exercise_23_12_20_1_一段错误的代码.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void GetMemory(char* p)
+{
+	p = (char*)malloc(100);
+}
+
+void Test(void)
+{
+	char* str = NULL;
+	GetMemory(str);
+	strcpy(str, "hello world");
+	printf(str);
+}
+
+int main()
+{
+	Test();
+	return 0;
+}
