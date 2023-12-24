2562becf6735c839e0b401903139eb9ef5e4d686
椋鸟Starling <2222040903@qq.com>
2023-12-24 20:04:29 +0800

    练习

--- C/exercise_23_12_05_6_枚举类型尝试/exercise_23_12_05_6_枚举类型尝试/exercise_23_12_05_6_枚举类型尝试.c
+++ C/exercise_23_12_05_6_枚举类型尝试/exercise_23_12_05_6_枚举类型尝试/exercise_23_12_05_6_枚举类型尝试.c
@@ -18,8 +18,11 @@ enum Sex
 
 int main()
 {
+	int a = GREEN;
+	printf("%d = %d\n", a, GREEN);
+
 	enum Color clr = GREEN;
-	clr = 2;//C:ok;C++:err	C++类型检查比较严格
+	clr = 2;	//C:ok;C++:err	C++类型检查比较严格
 
 	enum ENUM_A
 	{
@@ -28,9 +31,8 @@ int main()
 		Z1 = 255,
 		A1,
 		B1,
-	};
+	} enumB = B1, * penmB = &enumB;
 	enum ENUM_A enumA = Y1;
-	enum ENUM_A enumB = B1;
 	printf("%d %d\n", enumA, enumB);
 
 	return 0;
