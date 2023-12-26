8db38856d72d21888005464e33ef8cde6ed4f684
椋鸟Starling <2222040903@qq.com>
2023-12-26 18:46:09 +0800

    练习

---
+++ C/exercise_23_12_26_1_sscanf/exercise_23_12_26_1_sscanf/exercise_23_12_26_1_sscanf.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int a = 0;
+	char* arr = (int*)malloc(100 * sizeof(int));
+	char str[100] = { 0 };
+	sscanf(" 114514 afjoiweaj jiojl", "%d%s%s", &a, arr, str);
+	printf("%d - %s - %s", a, arr, str);
+	return 0;
+}
