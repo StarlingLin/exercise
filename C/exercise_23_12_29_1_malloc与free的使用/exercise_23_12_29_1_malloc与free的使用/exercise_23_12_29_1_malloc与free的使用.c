4114bd0c3a7e34d11c11ccc0f0fa29f138384619
椋鸟Starling <2222040903@qq.com>
2023-12-29 21:07:00 +0800

    练习

---
+++ C/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	int* ptr = NULL;
+	ptr = (int*)malloc(num * sizeof(int));
+	if (NULL != ptr)//≈–∂œptr÷∏’Î «∑ÒŒ™ø’
+	{
+		for (int i = 0; i < num; i++)
+		{
+			ptr[i] = i;
+		}
+		for (int i = 0; i < num; i++)
+		{
+			printf("%d ", ptr[i]);
+		}
+	}
+	free(ptr);
+	ptr = NULL;
+	return 0;
+}
