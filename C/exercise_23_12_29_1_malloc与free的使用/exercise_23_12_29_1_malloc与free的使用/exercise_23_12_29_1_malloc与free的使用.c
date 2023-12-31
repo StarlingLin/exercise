cf362cb50c544f257b8ad38931426709024e07dc
椋鸟Starling <2222040903@qq.com>
2023-12-31 16:25:28 +0800

    练习

--- C/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用.c
+++ C/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用/exercise_23_12_29_1_malloc与free的使用.c
@@ -8,17 +8,15 @@ int main()
 	scanf("%d", &num);
 	int* ptr = NULL;
 	ptr = (int*)malloc(num * sizeof(int));
-	if (NULL != ptr)//判断ptr指针是否为空
+	if (ptr == NULL)	//判断ptr指针是否为空
 	{
-		for (int i = 0; i < num; i++)
-		{
-			ptr[i] = i;
-		}
-		for (int i = 0; i < num; i++)
-		{
-			printf("%d ", ptr[i]);
-		}
+		perror("malloc");
+		return 1;
 	}
+	for (int i = 0; i < num; i++)
+		ptr[i] = i;
+	for (int i = 0; i < num; i++)
+		printf("%d ", ptr[i]);
 	free(ptr);
 	ptr = NULL;
 	return 0;
