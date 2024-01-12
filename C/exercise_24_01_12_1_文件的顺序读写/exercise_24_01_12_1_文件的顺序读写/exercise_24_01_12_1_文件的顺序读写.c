6f22870dd26e72dbc42f448000cd6bdb4ab30965
椋鸟Starling <2222040903@qq.com>
2024-01-12 22:22:28 +0800

    练习

--- C/exercise_24_01_12_1_文件的顺序读写/exercise_24_01_12_1_文件的顺序读写/exercise_24_01_12_1_文件的顺序读写.c
+++ C/exercise_24_01_12_1_文件的顺序读写/exercise_24_01_12_1_文件的顺序读写/exercise_24_01_12_1_文件的顺序读写.c
@@ -1,27 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    FILE* fp = fopen("test.txt", "r");
+//    if (!fp)
+//    {
+//        perror("File opening failed");
+//        return EXIT_FAILURE;
+//    }
+//
+//    int c;  // 注意：int，非char，要求处理EOF
+//    while ((c = fgetc(fp)) != EOF)  // 标准C I/O读取文件循环
+//    { 
+//        putchar(c);
+//    }
+//
+//    if (ferror(fp))
+//        puts("I/O error when reading");
+//    else if (feof(fp))
+//        puts("End of file reached successfully");
+//
+//    fclose(fp);
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    FILE* fp = fopen("test.txt", "r");
-    if (!fp)
-    {
-        perror("File opening failed");
-        return EXIT_FAILURE;
-    }
+    int ret_code = 0;
+    for (char c = 'a'; (ret_code != EOF) && (c != 'z'); c++)
+        ret_code = putc(c, stdout);
 
-    int c;  // 注意：int，非char，要求处理EOF
-    while ((c = fgetc(fp)) != EOF)  // 标准C I/O读取文件循环
-    { 
-        putchar(c);
-    }
+    /* 测试是否抵达 EOF */
+    if (ret_code == EOF)
+        if (ferror(stdout))
+        {
+            perror("putc()");
+            fprintf(stderr, "putc() failed in file %s at line # %d\n", __FILE__, __LINE__ - 7);
+            exit(EXIT_FAILURE);
+        }
+    putc('\n', stdout);
 
-    if (ferror(fp))
-        puts("I/O error when reading");
-    else if (feof(fp))
-        puts("End of file reached successfully");
-
-    fclose(fp);
+    return EXIT_SUCCESS;
 }
