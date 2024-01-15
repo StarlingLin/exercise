9a886b5f11c6730f978c5f17cb63cc7172407b35
StarlingLin <2222040903@qq.com>
2024-01-15 20:29:08 +0800

    练习

---
+++ C/exercise_24_01_15_1_编译与链接/test.s
@@ -0,0 +1,41 @@
+	.file	"test.c"
+	.text
+	.globl	g_val
+	.data
+	.align 4
+	.type	g_val, @object
+	.size	g_val, 4
+g_val:
+	.long	1919810
+	.section	.rodata
+.LC0:
+	.string	"114514"
+.LC1:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	g_val(%rip), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.1.0"
+	.section	.note.GNU-stack,"",@progbits
