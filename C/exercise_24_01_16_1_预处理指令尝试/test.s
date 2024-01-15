d9a979025c03edc2b2b011f583de3379c8411c83
StarlingLin <2222040903@qq.com>
2024-01-16 00:43:27 +0800

    练习

---
+++ C/exercise_24_01_16_1_预处理指令尝试/test.s
@@ -0,0 +1,64 @@
+	.file	"test.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"test.h"
+.LC1:
+	.string	"Jan 16 2024"
+.LC2:
+	.string	"00:42:09"
+.LC3:
+	.string	"%d\n"
+	.text
+	.globl	test
+	.type	test, @function
+test:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$.LC1, %edi
+	call	puts
+	movl	$.LC2, %edi
+	call	puts
+	movl	$8, %esi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$1, %esi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	printf
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	test, .-test
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %eax
+	call	test
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.1.0"
+	.section	.note.GNU-stack,"",@progbits
