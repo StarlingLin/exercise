2056db00bdbc15b7b2adced778c4348a66a0e49e
StarlingLin <2222040903@qq.com>
2024-01-31 09:25:11 +0800

    练习

--- C/exercise_24_01_16_3_预处理指令尝试3/test.c
+++ C/exercise_24_01_16_3_预处理指令尝试3/test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MALLOC(size, TYPE) ((TYPE*)malloc(size*sizeof(TYPE)))
+#define MALLOC(p, size, TYPE) TYPE *p = ((TYPE*)malloc(size*sizeof(TYPE)))
 
 int main()
 {
-    int *p = MALLOC(114514, int);
-    printf("%d", malloc_usable_size(p));
+    MALLOC(p, 114514, int);
+    printf("%d\n", malloc_usable_size(p));
     free(p);
     p = NULL;
     return 0;
