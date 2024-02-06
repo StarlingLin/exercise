b3866d83285cce0c40c9648f54d89d57fda9eb91
椋鸟Starling <2222040903@qq.com>
2024-02-06 21:20:02 +0800

    练习

---
+++ C/exercise_24_02_06_快慢指针找中间节点/exercise_24_02_06_快慢指针找中间节点/exercise_24_02_06_快慢指针找中间节点.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* middleNode(struct ListNode* head)
+{
+    struct ListNode* p1, * p2;
+    p1 = p2 = head;
+    while (p2 && p2->next)
+    {
+        p1 = p1->next;
+        p2 = p2->next->next;
+    }
+    return p1;
+}
+
+int main()
+{
+    return 0;
+}
