c0d20965e1ec4e2a82f120ec60830761e53c9842
椋鸟Starling <2222040903@qq.com>
2024-02-09 15:17:20 +0800

    练习

--- C/exercise_24_02_08_2_leetcode206/exercise_24_02_08_2_leetcode206/exercise_24_02_08_2_leetcode206.c
+++ C/exercise_24_02_08_2_leetcode206/exercise_24_02_08_2_leetcode206/exercise_24_02_08_2_leetcode206.c
@@ -28,25 +28,5 @@ ListNode* reverseList(ListNode* head)
 
 int main()
 {
-	ListNode* head = (ListNode*)malloc(sizeof(ListNode));
-	head->val = 1;
-	head->next = (ListNode*)malloc(sizeof(ListNode));
-	head->next->val = 2;
-	head->next->next = (ListNode*)malloc(sizeof(ListNode));
-	head->next->next->val = 3;
-	head->next->next->next = (ListNode*)malloc(sizeof(ListNode));
-	head->next->next->next->val = 4;
-	head->next->next->next->next = (ListNode*)malloc(sizeof(ListNode));
-	head->next->next->next->next->val = 5;
-	head->next->next->next->next->next = NULL;
-
-	ListNode* reversed = reverseList(head);
-
-	while (reversed != NULL)
-	{
-		printf("%d ", reversed->val);
-		reversed = reversed->next;
-	}
-
 	return 0;
 }
