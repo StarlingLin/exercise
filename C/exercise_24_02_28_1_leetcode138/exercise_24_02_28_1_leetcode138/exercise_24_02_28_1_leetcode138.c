7f80a00ab0c9b51b4b448251c5861b696b369c6e
椋鸟Starling <2222040903@qq.com>
2024-03-12 23:40:53 +0800

    暴力解法

--- C/exercise_24_02_28_1_leetcode138/exercise_24_02_28_1_leetcode138/exercise_24_02_28_1_leetcode138.c
+++ C/exercise_24_02_28_1_leetcode138/exercise_24_02_28_1_leetcode138/exercise_24_02_28_1_leetcode138.c
@@ -40,7 +40,119 @@ struct Node* copyRandomList1(struct Node* head)
 }
 
 //±©Á¦
+void PushBack(struct Node** head, int val)
+{
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+	new_node->val = val;
+	new_node->next = NULL;
+	new_node->random = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		struct Node* cur = *head;
+		while (cur->next)
+		{
+			cur = cur->next;
+		}
+		cur->next = new_node;
+	}
+}
+int GetIndex(struct Node* head, struct Node* node)
+{
+	if (node == NULL)
+	{
+		return -1;
+	}
+	int index = 0;
+	struct Node* cur = head;
+	while (cur)
+	{
+		if (cur == node)
+		{
+			return index;
+		}
+		cur = cur->next;
+		++index;
+	}
+	return -1;
+}
+struct Node* GetNode(struct Node* head, int index)
+{
+	if (index < 0)
+	{
+		return NULL;
+	}
+	struct Node* cur = head;
+	while (cur && index)
+	{
+		cur = cur->next;
+		--index;
+	}
+	return cur;
+}
 struct Node* copyRandomList2(struct Node* head) 
 {
+    if (head == NULL) 
+	{
+		return NULL;
+	}
+    struct Node* headNew = malloc(sizeof(struct Node));
+	headNew->val = head->val;
+    headNew->next = NULL;
+    headNew->random = NULL;
+    struct Node* cur = head->next;
+    while (cur != NULL)
+    {
+		PushBack(&headNew, cur->val);
+		cur = cur->next;
+	}
+	struct Node* curNew = headNew;
+	cur = head;
+	while (cur != NULL)
+	{
+		if (cur->random != NULL)
+		{
+			int index = GetIndex(head, cur->random);
+			curNew->random = GetNode(headNew, index);
+		}
+		cur = cur->next;
+		curNew = curNew->next;
+	}
+	return headNew;
+}
 
+int main()
+{
+	struct Node* head = NULL;
+	PushBack(&head, 7);
+	PushBack(&head, 13);
+	PushBack(&head, 11);
+	PushBack(&head, 10);
+	PushBack(&head, 1);
+	head->random = NULL;
+	head->next->random = head;
+	head->next->next->random = head->next->next->next->next;
+	head->next->next->next->random = head->next->next;
+	head->next->next->next->next->random = head;
+	struct Node* headNew = copyRandomList2(head);
+	struct Node* cur = headNew;
+	while (cur)
+	{
+		printf("%d,", cur->val);
+		if (cur->random)
+		{
+			printf("%d ", GetIndex(headNew, cur->random));
+		}
+		else
+		{
+			printf("NULL ");
+		}
+		cur = cur->next;
+	}
+	printf("\n");
+	return 0;
 }
