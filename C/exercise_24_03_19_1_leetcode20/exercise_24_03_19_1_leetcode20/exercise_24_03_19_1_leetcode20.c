6c8ed7003379239c19215e1897d35b7bbde01fe8
椋鸟Starling <2222040903@qq.com>
2024-03-19 21:24:46 +0800

    练习

---
+++ C/exercise_24_03_19_1_leetcode20/exercise_24_03_19_1_leetcode20/exercise_24_03_19_1_leetcode20.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Stack
+{
+    char* data;
+    int top;
+    int capacity;
+} Stack;
+
+void StackInit(Stack* st)
+{
+    st->data = NULL;
+    st->top = st->capacity = 0;
+}
+
+void StackPush(Stack* st, char data)
+{
+    if (st->top == st->capacity)
+    {
+        int newcapacity = (st->capacity == 0 ? 4 : 2 * st->capacity);
+        char* tmp = (char*)realloc(st->data, newcapacity);
+        if (tmp == NULL)
+        {
+            exit(1);
+        }
+        st->data = tmp;
+        st->capacity = newcapacity;
+    }
+    st->data[st->top] = data;
+    st->top++;
+}
+
+char StackPop(Stack* st)
+{
+    char ret = st->data[st->top - 1];
+    st->top--;
+    return ret;
+}
+
+bool isValid(char* s) {
+    Stack st;
+    StackInit(&st);
+    char* temp = s;
+
+    while (*temp != '\0')
+    {
+        if (*temp == '(' || *temp == '[' || *temp == '{')
+        {
+            StackPush(&st, *temp);
+        }
+        else if (*temp == ')' || *temp == ']' || *temp == '}')
+        {
+            char get = StackPop(&st);
+            if (!(get == '(' && *temp == ')') &&
+                !(get == '[' && *temp == ']') && 
+                !(get == '{' && *temp == '}'))
+            {
+                return false;
+            }
+        }
+        temp++;
+    }
+    return true;
+}
+
+int main()
+{
+	char* s = "([[][]])[][][][[{}]]";
+	printf("%s\n", isValid(s) ? "true" : "false");
+	return 0;
+}
