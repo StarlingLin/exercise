58d9d1dc904e78b3b516b8c581b5a29b34773702
椋鸟Starling <2222040903@qq.com>
2024-03-28 23:38:00 +0800

    练习

--- C/exercise_24_03_27_1_二叉树实现/exercise_24_03_27_1_二叉树实现/BinaryTree.c
+++ C/exercise_24_03_27_1_二叉树实现/exercise_24_03_27_1_二叉树实现/BinaryTree.c
@@ -42,21 +42,93 @@ void BinaryTreeDestory(BTNode** root)
 // 二叉树节点个数
 int BinaryTreeSize(BTNode* root)
 {
-
+	if (root == NULL)
+	{
+		return 0;
+	}
+	return 1 + BinaryTreeSize(root->_left) + BinaryTreeSize(root->_right);
 }
 // 二叉树叶子节点个数
-int BinaryTreeLeafSize(BTNode* root);
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	if (root->_left == NULL && root->_right == NULL)
+	{
+		return 1;
+	}
+	return BinaryTreeLeafSize(root->_left) + BinaryTreeLeafSize(root->_right);
+}
 // 二叉树第k层节点个数
-int BinaryTreeLevelKSize(BTNode* root, int k);
+int BinaryTreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	if (k == 1)
+	{
+		return 1;
+	}
+	return BinaryTreeLevelKSize(root->_left, k - 1) + BinaryTreeLevelKSize(root->_right, k - 1);
+}
 // 二叉树查找值为x的节点
-BTNode* BinaryTreeFind(BTNode* root, BTDataType x);
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	if (root->_data == x)
+	{
+		return root;
+	}
+	BTNode* ret = BinaryTreeFind(root->_left, x);
+	if (ret != NULL)
+	{
+		return ret;
+	}
+	return BinaryTreeFind(root->_right, x);
+}
 // 二叉树前序遍历 
-void BinaryTreePrevOrder(BTNode* root);
+void BinaryTreePrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	printf("%c ", root->_data);
+	BinaryTreePrevOrder(root->_left);
+	BinaryTreePrevOrder(root->_right);
+}
 // 二叉树中序遍历
-void BinaryTreeInOrder(BTNode* root);
+void BinaryTreeInOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	BinaryTreeInOrder(root->_left);
+	printf("%c ", root->_data);
+	BinaryTreeInOrder(root->_right);
+}
 // 二叉树后序遍历
-void BinaryTreePostOrder(BTNode* root);
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	BinaryTreePostOrder(root->_left);
+	BinaryTreePostOrder(root->_right);
+	printf("%c ", root->_data);
+}
 // 层序遍历
-void BinaryTreeLevelOrder(BTNode* root);
+void BinaryTreeLevelOrder(BTNode* root)
+{
+
+}
 // 判断二叉树是否是完全二叉树
 int BinaryTreeComplete(BTNode* root);
