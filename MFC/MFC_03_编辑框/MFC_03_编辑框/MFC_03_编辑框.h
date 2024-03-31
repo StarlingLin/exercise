ad61f35e032d1d6ede4c5cf7fee861e0fbe7274b
椋鸟Starling <2222040903@qq.com>
2024-04-01 00:05:48 +0800

    练习

---
+++ MFC/MFC_03_编辑框/MFC_03_编辑框/MFC_03_编辑框.h
@@ -0,0 +1,32 @@
+﻿
+// MFC_03_编辑框.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC03编辑框App:
+// 有关此类的实现，请参阅 MFC_03_编辑框.cpp
+//
+
+class CMFC03编辑框App : public CWinApp
+{
+public:
+	CMFC03编辑框App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC03编辑框App theApp;
