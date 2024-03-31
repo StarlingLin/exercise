774e2c9e5f46387beb8d450776121ba638e33ff7
椋鸟Starling <2222040903@qq.com>
2024-03-31 22:52:37 +0800

    练习

---
+++ MFC/MFC_01_模态与非模态对话框/MFC_01_模态与非模态对话框/MFC_01_模态与非模态对话框.h
@@ -0,0 +1,32 @@
+﻿
+// MFC_01_模态与非模态对话框.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC01模态与非模态对话框App:
+// 有关此类的实现，请参阅 MFC_01_模态与非模态对话框.cpp
+//
+
+class CMFC01模态与非模态对话框App : public CWinApp
+{
+public:
+	CMFC01模态与非模态对话框App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC01模态与非模态对话框App theApp;
---
+++ MFC/MFC_01_模态与非模态对话框/MFC_01_模态与非模态对话框/MFC_01_模态与非模态对话框Dlg.h
@@ -0,0 +1,40 @@
+﻿
+// MFC_01_模态与非模态对话框Dlg.h: 头文件
+//
+
+#pragma once
+
+#include "CDlgShow.h"
+
+// CMFC01模态与非模态对话框Dlg 对话框
+class CMFC01模态与非模态对话框Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC01模态与非模态对话框Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_01__DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton1();
+private:
+	CDlgShow m_dlgShow;
+};
