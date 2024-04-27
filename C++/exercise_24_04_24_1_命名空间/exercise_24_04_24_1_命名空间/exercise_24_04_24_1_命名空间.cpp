d6e1cc22e806041625b209a2c02030ffed968434
椋鸟Starling <2222040903@qq.com>
2024-04-27 18:42:14 +0800

    练习

--- C++/exercise_24_04_24_1_命名空间/exercise_24_04_24_1_命名空间/exercise_24_04_24_1_命名空间.cpp
+++ C++/exercise_24_04_24_1_命名空间/exercise_24_04_24_1_命名空间/exercise_24_04_24_1_命名空间.cpp
@@ -4,43 +4,105 @@
 
 using namespace std;
 
-namespace A
-{
-	int a = 10;
-	void fun()
-	{
-		cout << "A::fun()" << endl;
-	}
-}
+//int a = 6;
+//
+//namespace A
+//{
+//	int a = 10;
+//	void fun()
+//	{
+//		cout << "A::fun()" << endl;
+//	}
+//}
+//
+//namespace B
+//{
+//	int a = 20;
+//	void fun()
+//	{
+//		cout << "B::fun()" << endl;
+//	}
+//
+//	namespace C
+//	{
+//		int a = 30;
+//		void fun()
+//		{
+//			cout << "C::fun()" << endl;
+//		}
+//	
+//	}
+//}
+//
+//int main()
+//{
+//	cout << a << endl;
+//	cout << ::a << endl;
+//	cout << A::a << endl;
+//	cout << B::a << endl;
+//	cout << B::C::a << endl;
+//
+//	A::fun();
+//	B::fun();
+//	B::C::fun();
+//
+//	return 0;
+//}
 
-namespace B
-{
-	int a = 20;
-	void fun()
-	{
-		cout << "B::fun()" << endl;
-	}
+//int a;
+//
+//namespace ExampleSpace1
+//{
+//	int a;
+//	int b;
+//}
+//
+//namespace ExampleSpace2
+//{
+//	int c;
+//	namespace ExampleSpace3
+//	{
+//		int d;
+//	}
+//}
 
-	namespace C
-	{
-		int a = 30;
-		void fun()
-		{
-			cout << "C::fun()" << endl;
-		}
-	
-	}
-}
+//using ExampleSpace1::b;
+//
+//int main()
+//{
+//	cout << b << endl;
+//	return 0;
+//}
 
-int main()
-{
-	cout << A::a << endl;
-	cout << B::a << endl;
-	cout << B::C::a << endl;
+//using namespace ExampleSpace2;
+//
+//int main()
+//{
+//	cout << ExampleSpace3::d << endl;
+//	return 0;
+//}
 
-	A::fun();
-	B::fun();
-	B::C::fun();
+//int main()
+//{
+//	if (true)
+//	{
+//		using namespace ExampleSpace2::ExampleSpace3;
+//		cout << d << endl;
+//	}
+//}
 
-	return 0;
-}
+//int a = 0;
+//
+//namespace X
+//{
+//	int a = 10;
+//}
+//
+//int main()
+//{
+//	if (true)
+//	{
+//		using namespace X;
+//		cout << a << endl;	//err
+//	}
+//}
