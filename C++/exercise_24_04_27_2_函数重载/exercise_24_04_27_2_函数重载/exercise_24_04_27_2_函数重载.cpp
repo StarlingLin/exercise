09bc8c5bc23aeafcd31ff55a2b699f0a9dc0510c
椋鸟Starling <2222040903@qq.com>
2024-04-28 00:39:03 +0800

    练习

--- C++/exercise_24_04_27_2_函数重载/exercise_24_04_27_2_函数重载/exercise_24_04_27_2_函数重载.cpp
+++ C++/exercise_24_04_27_2_函数重载/exercise_24_04_27_2_函数重载/exercise_24_04_27_2_函数重载.cpp
@@ -4,26 +4,104 @@
 
 using namespace std;
 
-void print(int a) 
-{
-	cout << "int: " << a << endl;
-}
+//void print(int a) 
+//{
+//	cout << "int: " << a << endl;
+//}
+//
+//void print(double a)
+//{
+//	cout << "double: " << a << endl;
+//}
+//
+//void print(char a)
+//{
+//	cout << "char: " << a << endl;
+//}
+//
+//int main()
+//{
+//	print(10);
+//	print(10.5);
+//	print('A');
+//
+//	return 0;
+//}
 
-void print(double a)
-{
-	cout << "double: " << a << endl;
-}
+//int& Count()
+//{
+//	static int n = 0;
+//	n++;
+//	return n;
+//}
+//
+//int main()
+//{
+//	int a = Count();
+//	int b = Count();
+//	int c = Count();
+//
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl;
+//
+//	return 0;
+//}
 
-void print(char a)
-{
-	cout << "char: " << a << endl;
-}
+//int& getElement(int* array, int index)
+//{
+//	return array[index];  // 返回数组元素的引用
+//}
+//
+//int main()
+//{
+//	int arr[3] = { 1, 2, 3 };
+//	getElement(arr, 0) = 4;  // 通过引用修改数组元素
+//	cout << arr[0] << endl;  // 输出 4
+//	return 0;
+//}
+
+//int& Add(int a, int b)
+//{
+//	int c = a + b;
+//	return c;
+//}
+//
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	Add(3, 4);
+//	cout << "Add(1,2) is: " << ret << endl;
+//	return 0;
+//}
+
+#include <time.h>
+
+struct A
+{ 
+	int a[10000]; 
+};
+
+A a;
+
+A TestFunc1() { return a; }
+A& TestFunc2() { return a; }
 
 int main()
 {
-	print(10);
-	print(10.5);
-	print('A');
+	// 以值作为函数的返回值类型
+	size_t begin1 = clock();
+	for (size_t i = 0; i < 1000000; ++i)
+		TestFunc1();
+	size_t end1 = clock();
+	// 以引用作为函数的返回值类型
+	size_t begin2 = clock();
+	for (size_t i = 0; i < 1000000; ++i)
+		TestFunc2();
+	size_t end2 = clock();
+	// 计算两个函数运算完成之后的时间
+	cout << "TestFunc1 time:" << end1 - begin1 << endl;
+	cout << "TestFunc2 time:" << end2 - begin2 << endl;
 
 	return 0;
 }
