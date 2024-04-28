f95f8a248cabd790568b2576e6f5adefd2a3b780
椋鸟Starling <2222040903@qq.com>
2024-04-28 18:49:01 +0800

    练习

---
+++ C++/exercise_24_04_28_3_范围for循环/exercise_24_04_28_3_范围for循环/exercise_24_04_28_3_范围for循环.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+void TestForLoop()
+{
+	int arr[] = { 1, 2, 3, 4, 5 };
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		arr[i] *= 2;
+	}
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+void TestForLoopWithRange()
+{
+	int arr[] = { 1, 2, 3, 4, 5 };
+	for (int& e : arr)
+	{
+		e *= 2;
+	}
+	for (int e : arr)
+	{
+		cout << e << " ";
+	}
+}
+
+int main()
+{
+	TestForLoop();
+	cout << endl;
+	TestForLoopWithRange();
+	return 0;
+}
