dc71e737058eff721adff79643eca744d477a797
椋鸟Starling <2222040903@qq.com>
2024-05-05 16:45:14 +0800

    练习

--- C++/exercise_24_04_30_1_类与对象/exercise_24_04_30_1_类与对象/exercise_24_04_30_1_类与对象.cpp
+++ C++/exercise_24_04_30_1_类与对象/exercise_24_04_30_1_类与对象/exercise_24_04_30_1_类与对象.cpp
@@ -36,6 +36,39 @@ using namespace std;
 
 //类的六个默认成员函数
 //1.构造函数
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//	{
+//		m_year = year;
+//		m_month = month;
+//		m_day = day;
+//	}
+//
+//	void print()
+//	{
+//		cout << m_year << "-" << m_month << "-" << m_day << endl;
+//	}
+//
+//private:
+//	int m_year;
+//	int m_month;
+//	int m_day;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//	d1.print();
+//
+//	Date d2(2020, 4, 30);
+//	d2.print();
+//
+//	return 0;
+//}
+
+//2.析构函数
 class Date
 {
 public:
@@ -44,6 +77,11 @@ public:
 		m_year = year;
 		m_month = month;
 		m_day = day;
+	}
+
+	~Date()
+	{
+		cout << "析构函数" << endl;
 	}
 
 	void print()
@@ -68,7 +106,6 @@ int main()
 	return 0;
 }
 
-//2.析构函数
 //3.拷贝构造函数
 //4.拷贝赋值运算符
 //5.移动构造函数
