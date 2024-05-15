fdcd6763564c8f9ec10b224d0efaa40698b9ba13
椋鸟Starling <2222040903@qq.com>
2024-05-15 23:19:40 +0800

    练习

---
+++ C++/exercise_24_05_15_1_初始化列表/exercise_24_05_15_1_初始化列表/exercise_24_05_15_1_初始化列表.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+class Date
+{
+public:
+	Date(int year, int month, int day)
+		: year(year)
+		, month(month)
+		, day(day)
+	{}
+
+	void print()
+	{
+		cout << year << "-" << month << "-" << day << endl;
+	}
+
+private:
+	int year;
+	int month;
+	int day;
+};
+
+int main()
+{
+	Date date(2015, 5, 24);
+	date.print();
+
+	return 0;
+}
