b7f579dba25a630950c8279a5e1b17400cdce33c
椋鸟Starling <2222040903@qq.com>
2024-06-06 23:15:51 +0800

    练习

---
+++ C++/exercise_24_06_06_1_模板/exercise_24_06_06_1_模板/exercise_24_06_06_1_模板.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void Swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int a = 10, b = 20;
+	cout << "a = " << a << ", b = " << b << endl;
+	Swap(a, b);
+	cout << "a = " << a << ", b = " << b << endl;
+
+	double c = 1.1, d = 2.2;
+	cout << "c = " << c << ", d = " << d << endl;
+	Swap(c, d);
+	cout << "c = " << c << ", d = " << d << endl;
+
+	return 0;
+}
