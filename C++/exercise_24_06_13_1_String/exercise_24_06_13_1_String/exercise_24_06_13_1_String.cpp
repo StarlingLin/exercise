2f51a074278654ab7b80268b39e957d9d110fa5c
椋鸟Starling <2222040903@qq.com>
2024-06-18 23:48:05 +0800

    练习

--- C++/exercise_24_06_13_1_String/exercise_24_06_13_1_String/exercise_24_06_13_1_String.cpp
+++ C++/exercise_24_06_13_1_String/exercise_24_06_13_1_String/exercise_24_06_13_1_String.cpp
@@ -36,47 +36,76 @@ using namespace std;
 //	return 0;
 //}
 
+//class Solution {
+//public:
+//	bool isLetter(char ch)
+//	{
+//		if (ch >= 'a' && ch <= 'z')
+//			return true;
+//		if (ch >= 'A' && ch <= 'Z')
+//			return true;
+//		return false;
+//	}
+//	string reverseOnlyLetters(string S) {
+//		if (S.empty())
+//			return S;
+//
+//		size_t begin = 0, end = S.size() - 1;
+//		while (begin < end)
+//		{
+//			while (begin < end && !isLetter(S[begin]))
+//				++begin;
+//
+//			while (begin < end && !isLetter(S[end]))
+//				--end;
+//			swap(S[begin], S[end]);
+//			++begin;
+//			--end;
+//		}
+//		return S;
+//	}
+//};
+//
+//int main()
+//{
+//	Solution solution;
+//	string str("ab-cd");
+//	cout << solution.reverseOnlyLetters(str) << endl;
+//
+//	str = "a-bC-dEf-ghIj";
+//	cout << solution.reverseOnlyLetters(str) << endl;
+//
+//	str = "Test1ng-Leet=code-Q!";
+//	cout << solution.reverseOnlyLetters(str) << endl;
+//
+//	return 0;
+//}
+
 class Solution {
 public:
-	bool isLetter(char ch)
-	{
-		if (ch >= 'a' && ch <= 'z')
-			return true;
-		if (ch >= 'A' && ch <= 'Z')
-			return true;
-		return false;
-	}
-	string reverseOnlyLetters(string S) {
-		if (S.empty())
-			return S;
+	int firstUniqChar(string s) {
+		// 统计每个字符出现的次数
+		int count[256] = { 0 };
+		int size = s.size();
+		for (int i = 0; i < size; ++i)
+			count[s[i]] += 1;
 
-		size_t begin = 0, end = S.size() - 1;
-		while (begin < end)
-		{
-			while (begin < end && !isLetter(S[begin]))
-				++begin;
-
-			while (begin < end && !isLetter(S[end]))
-				--end;
-			swap(S[begin], S[end]);
-			++begin;
-			--end;
-		}
-		return S;
+		// 按照字符次序从前往后找只出现一次的字符
+		for (int i = 0; i < size; ++i)
+			if (1 == count[s[i]])
+				return i;
+		return -1;
 	}
 };
 
 int main()
 {
 	Solution solution;
-	string str("ab-cd");
-	cout << solution.reverseOnlyLetters(str) << endl;
+	string str("leetcode");
+	cout << solution.firstUniqChar(str) << endl;
 
-	str = "a-bC-dEf-ghIj";
-	cout << solution.reverseOnlyLetters(str) << endl;
-
-	str = "Test1ng-Leet=code-Q!";
-	cout << solution.reverseOnlyLetters(str) << endl;
+	str = "loveleetcode";
+	cout << solution.firstUniqChar(str) << endl;
 
 	return 0;
 }
