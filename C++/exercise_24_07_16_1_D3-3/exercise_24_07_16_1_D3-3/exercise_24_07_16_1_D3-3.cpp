282634c4cce424d00a2b29554a4474538ba38a29
椋鸟Starling <2222040903@qq.com>
2024-07-16 22:12:33 +0800

    练习

--- C++/exercise_24_07_16_1_D3-3/exercise_24_07_16_1_D3-3/exercise_24_07_16_1_D3-3.cpp
+++ C++/exercise_24_07_16_1_D3-3/exercise_24_07_16_1_D3-3/exercise_24_07_16_1_D3-3.cpp
@@ -5,55 +5,110 @@
 //第一行输入两个整数n和k(1<=n<=10^5, 1<=k<=10^9)。
 //第二行输入n个整数，表示数组中的数，每个数不超过10^9。
 
+//复杂度过高，无法通过测试用例
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int minSumAfterOperations(int n, int k, vector<int>& arr) 
+//{
+//    int sum = 0;
+//
+//    for (int num : arr) 
+//    {
+//        sum += num;
+//    }
+//
+//    for (int i = 0; i < k; ++i) 
+//    {
+//        int max_even_index = -1;
+//        for (int j = 0; j < n; ++j) 
+//        {
+//            if (arr[j] % 2 == 0 && (max_even_index == -1 || arr[j] > arr[max_even_index])) 
+//            {
+//                max_even_index = j;
+//            }
+//        }
+//
+//        if (max_even_index == -1) 
+//        {
+//            break;
+//        }
+//
+//        sum -= arr[max_even_index] / 2;
+//        arr[max_even_index] /= 2;
+//    }
+//
+//    return sum;
+//}
+//
+//int main() 
+//{
+//    int n, k;
+//    cin >> n >> k;
+//    vector<int> arr(n);
+//
+//    for (int i = 0; i < n; ++i) 
+//    {
+//        cin >> arr[i];
+//    }
+//
+//    cout << minSumAfterOperations(n, k, arr) << endl;
+//    return 0;
+//}
+
+//建立最大堆存放偶数，每次取出最大值，除以2，若为偶数再放回堆中，最多k次操作
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-long long minSumAfterOperations(int n, int k, vector<int>& arr) 
+long long minSumAfterOperations(int n, int k, vector<int>& arr)
 {
-    long long sum = 0;
+	long long sum = 0;
+	priority_queue<int> pq;
 
-    for (int num : arr) 
-    {
-        sum += num;
-    }
+	for (int num : arr)
+	{
+		sum += num;
+		if (num % 2 == 0)
+		{
+			pq.push(num);
+		}
+	}
 
-    for (int i = 0; i < k; ++i) 
-    {
-        int max_even_index = -1;
-        for (int j = 0; j < n; ++j) 
-        {
-            if (arr[j] % 2 == 0 && (max_even_index == -1 || arr[j] > arr[max_even_index])) 
-            {
-                max_even_index = j;
-            }
-        }
+	for (int i = 0; i < k; ++i)
+	{
+		if (pq.empty())
+		{
+			break;
+		}
 
-        if (max_even_index == -1) 
-        {
-            break;
-        }
+		int max_even = pq.top();
+		pq.pop();
+		sum -= max_even / 2;
+		if (max_even / 2 % 2 == 0)
+			pq.push(max_even / 2);
+	}
 
-        sum -= arr[max_even_index] / 2;
-        arr[max_even_index] /= 2;
-    }
-
-    return sum;
+	return sum;
 }
 
-int main() 
+int main()
 {
-    int n, k;
-    cin >> n >> k;
-    vector<int> arr(n);
+	int n, k;
+	cin >> n >> k;
+	vector<int> arr(n);
 
-    for (int i = 0; i < n; ++i) 
-    {
-        cin >> arr[i];
-    }
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
 
-    cout << minSumAfterOperations(n, k, arr) << endl;
-    return 0;
+	cout << minSumAfterOperations(n, k, arr) << endl;
+	return 0;
 }
