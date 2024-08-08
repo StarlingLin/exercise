ad32e7cc3bb9caa48f0ff54f8d9fa4b65ef5c961
椋鸟Starling <2222040903@qq.com>
2024-08-08 22:01:37 +0800

    练习

---
+++ C++/exercise_24_08_08_3_D21-3/exercise_24_08_08_3_D21-3/exercise_24_08_08_3_D21-3.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//寻找最长回文子串
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int n = s.size();
+	int max_len = 0;
+	int start = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int l = i, r = i;
+		while (l >= 0 && r < n && s[l] == s[r])
+		{
+			l--;
+			r++;
+		}
+		if (r - l - 1 > max_len)
+		{
+			max_len = r - l - 1;
+			start = l + 1;
+		}
+		l = i;
+		r = i + 1;
+		while (l >= 0 && r < n && s[l] == s[r])
+		{
+			l--;
+			r++;
+		}
+		if (r - l - 1 > max_len)
+		{
+			max_len = r - l - 1;
+			start = l + 1;
+		}
+	}
+	cout << s.substr(start, max_len) << endl;
+
+	return 0;
+}
