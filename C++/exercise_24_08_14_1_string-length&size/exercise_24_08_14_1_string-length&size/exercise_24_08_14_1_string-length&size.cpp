a49ec3bf531d6a801c118888f8bd321e6c6144ac
椋鸟Starling <2222040903@qq.com>
2024-08-14 15:09:55 +0800

    练习

---
+++ C++/exercise_24_08_14_1_string-length&size/exercise_24_08_14_1_string-length&size/exercise_24_08_14_1_string-length&size.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s("Hello, World!");
+	cout << s.size() << endl;
+	cout << s.length() << endl;
+
+	return 0;
+}
