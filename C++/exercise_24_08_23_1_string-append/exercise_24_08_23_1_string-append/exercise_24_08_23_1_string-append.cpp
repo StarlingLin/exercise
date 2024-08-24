b74b5296ee2847b7ccebd9274018f236d89b5ad7
椋鸟Starling <2222040903@qq.com>
2024-08-24 17:18:02 +0800

    练习

--- C++/exercise_24_08_23_1_string-append/exercise_24_08_23_1_string-append/exercise_24_08_23_1_string-append.cpp
+++ C++/exercise_24_08_23_1_string-append/exercise_24_08_23_1_string-append/exercise_24_08_23_1_string-append.cpp
@@ -5,20 +5,30 @@
 
 using namespace std;
 
+//int main()
+//{
+//	string s = "append:";
+//	string t = "uvwxyz";
+//	s.append(t);		// 将字符串t的内容追加到字符串s的末尾
+//	cout << s << endl;	// append:uvwxyz
+//	s.append(t, 2, 3);	// 将字符串t的子串（从t[2]开始数3位）追加到字符串s的末尾
+//	cout << s << endl;	// append:uvwxyzwxy
+//	s.append("123");	// 将字符串"123"追加到字符串s的末尾
+//	cout << s << endl;	// append:uvwxyzwxy123
+//	s.append("456", 2);	// 将字符串"456"的前2个字符追加到字符串s的末尾
+//	cout << s << endl;	// append:uvwxyzwxy12345
+//	s.append(3, 'x');	// 将字符'x'追加到字符串s的末尾3次
+//	cout << s << endl;	// append:uvwxyzwxy12345xxx
+//
+//	return 0; 
+//}
+
 int main()
 {
 	string s = "append:";
-	string t = "uvwxyz";
-	s.append(t);		// 将字符串t的内容追加到字符串s的末尾
-	cout << s << endl;	// append:uvwxyz
-	s.append(t, 2, 3);	// 将字符串t的子串（从t[2]开始数3位）追加到字符串s的末尾
-	cout << s << endl;	// append:uvwxyzwxy
-	s.append("123");	// 将字符串"123"追加到字符串s的末尾
-	cout << s << endl;	// append:uvwxyzwxy123
-	s.append("456", 2);	// 将字符串"456"的前2个字符追加到字符串s的末尾
-	cout << s << endl;	// append:uvwxyzwxy12345
-	s.append(3, 'x');	// 将字符'x'追加到字符串s的末尾3次
-	cout << s << endl;	// append:uvwxyzwxy12345xxx
+	cout << (s += "abc") << endl;	// append:abc
+	cout << (s += s) << endl;		// append:abcappend:abc
+	cout << (s += '.') << endl;		// append:abcappend:abc.
 
-	return 0; 
+	return 0;
 }
