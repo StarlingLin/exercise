18d97433a2a882dbfa67b040a6a4d3f806b32efb
StarlingLin <2222040903@qq.com>
2025-03-27 17:47:07 +0800

    练习

---
+++ Java/exercise_25_03_27_1_exp2-6/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("斯派克", 3);
+        Cat cat = new Cat("汤姆", 2);
+        dog.makeSound();
+        cat.makeSound();
+    }
+}
